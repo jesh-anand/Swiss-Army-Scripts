46d9f92e2c237f13a9b7d2d90141ab3074450ec8
Ananthan <ANAN011@my.arvato-systems.com>
2016-10-10 11:01:21 +0800

    Modified files

--- Spring_Javabrains/src/main/java/org/prajesh/javabrains/aop/main/AOPMain.java
+++ Spring_Javabrains/src/main/java/org/prajesh/javabrains/aop/main/AOPMain.java
@@ -6,14 +6,13 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
  * Main class for AOP implementation
- * 
- * @author Prajesh Ananthan
  *
+ * @author Prajesh Ananthan
  */
 public class AOPMain {
     public static void main(String[] args) {
-	ApplicationContext ctx = new ClassPathXmlApplicationContext("spring-aop.xml");
-	ShapeService shapeService = ctx.getBean("shapeService", ShapeService.class);
-	System.out.println(shapeService.getCircle().getName());
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("spring-aop.xml");
+        ShapeService shapeService = ctx.getBean("shapeService", ShapeService.class);
+        System.out.println(shapeService.getCircle().getName());
     }
 }
